57f742ec0a331e1bce0ff8f9a74a5fab0c4aaa7b
jesopo <github@lolnerd.net>
2020-07-01 18:02:16 +0100

    fix glob collapse tests

--- test/glob.py
+++ test/glob.py
@@ -3,14 +3,14 @@ from ircrobots import glob
 
 class GlobTestCollapse(unittest.TestCase):
     def test(self):
-        c1 = glob._collapse("**?*")
+        c1 = glob.collapse("**?*")
         self.assertEqual(c1, "?*")
 
-        c2 = glob._collapse("a**?a*")
+        c2 = glob.collapse("a**?a*")
         self.assertEqual(c2, "a?*a*")
 
-        c3 = glob._collapse("?*?*?*?*a")
+        c3 = glob.collapse("?*?*?*?*a")
         self.assertEqual(c3, "????*a")
 
-        c4 = glob._collapse("a*?*a?**")
+        c4 = glob.collapse("a*?*a?**")
         self.assertEqual(c4, "a?*a?*")
