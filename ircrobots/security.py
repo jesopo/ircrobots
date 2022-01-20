8245a411c0407e36049a61c2932fd2ac59698a7e
jesopo <github@lolnerd.net>
2022-01-20 21:24:54 +0000

    hmm no this isnt how you ask for cert validation apparently

--- ircrobots/security.py
+++ ircrobots/security.py
@@ -1,13 +1,4 @@
 import ssl
 
 def tls_context(verify: bool=True) -> ssl.SSLContext:
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-    context.options |= ssl.OP_NO_SSLv2
-    context.options |= ssl.OP_NO_SSLv3
-    context.options |= ssl.OP_NO_TLSv1
-    context.load_default_certs()
-
-    if verify:
-        context.verify_mode = ssl.CERT_REQUIRED
-
-    return context
+    return ssl.create_default_context()
